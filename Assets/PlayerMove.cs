7fa672e9746a9b1fa0f537badf5205bf0f4b68dc
Jack Fenton <jack.fenton@thoughtworks.com>
2019-06-18 16:04:51 +1000

    Create NightTileset and basic scene structures for testing. Added basic movement and animation scripts to player

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    public float Speed = 10f;
+    private bool FacingRight;
+    private float horizontalinput;
+
+	// Use this for initialization
+	void Start () {
+        FacingRight = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        horizontalinput = Input.GetAxis("Horizontal");
+        float rightorleft = horizontalinput * Speed * Time.deltaTime;
+        transform.position = new Vector2(transform.position.x + rightorleft, transform.position.y);
+        Flip(horizontalinput);
+	}
+
+    //Flips the player x scale if the horizontal value (direction) is not the same as the 
+    //player orientation
+    void Flip(float horizontal)
+    {
+        if((horizontal > 0.1 && !FacingRight) || (horizontal < -0.1 && FacingRight))
+        {
+            FacingRight = !FacingRight;
+
+            Vector3 playerScale = transform.localScale;
+            playerScale.x *= -1;
+            transform.localScale = playerScale;
+        }
+    }
+}
