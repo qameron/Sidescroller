271dd8c466fdb98dd862e5a989b312abfefb13a0
Cameron <cameron.warton@students.mq.edu.au>
2019-06-20 02:41:43 +1000

    added ultra basic enemy movement.

---
+++ Assets/EnemyMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour {
+
+  //  [SerializeField]
+    public float speed = 1f;
+
+    private Vector2 direction;
+
+	// Use this for initialization
+	void Start () {
+        direction = Vector2.right;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();	
+	}
+
+    public void Move()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
