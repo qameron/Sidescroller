84a6d9862fceec519fba26a366f655c26afd56f1
Cameron <cameron.warton@students.mq.edu.au>
2019-06-20 04:13:21 +1000

    patrol mode is perfect now with customisable patrol range. need to implement a circle collider sight cone to 'search' for the player and ability to attack.

--- Assets/EnemyMove.cs
+++ Assets/EnemyMove.cs
@@ -3,24 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMove : MonoBehaviour {
-
-  //  [SerializeField]
-    public float speed = 1f;
-
-    private Vector2 direction;
+    private Rigidbody2D rb;
+    private float speed = 0.5f;
+    private Animator anim;
+    //private float rightorleft;
+    private Vector2 direction = Vector2.right;
+    public float PatrolRadius = 1000;
+    private float IdleTimer = 0;
+    private float Ptimer = 0;
+   // public bool FaceRight = true;
+    
 
 	// Use this for initialization
-	void Start () {
-        direction = Vector2.right;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Move();	
+	void Start ()
+    {
+            anim = GetComponent<Animator>();
+            direction = Vector2.right;
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        rb = GetComponent<Rigidbody2D>();
+        Patrol();	
 	}
 
-    public void Move()
+    public void Patrol ()
     {
-        transform.Translate(direction * speed * Time.deltaTime);
+
+        
+
+        if (Ptimer < PatrolRadius){
+            
+            
+            transform.Translate(direction * speed * Time.deltaTime);
+            Ptimer += 15;
+            anim.SetFloat("walk", direction.x);
+            
+        } else
+        {
+            Ptimer = -1000;
+            if (direction == Vector2.right)
+            {
+                direction = Vector2.left;
+                Vector3 playerScale = transform.localScale;
+                playerScale.x *= -1;
+                transform.localScale = playerScale;
+            }
+            else
+            {
+                direction = Vector2.right;
+                Vector3 playerScale = transform.localScale;
+                playerScale.x *= -1;
+                transform.localScale = playerScale;
+            }
+        }
+
+       
+         
+
     }
+
 }
